ceea7918a7c5e80d525b4056dc2da565efee35c6
Pavan Baloju <pavanbaloju9@gmail.com>
2023-01-06 14:47:10 +0530

    Add rank component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
 import FaceRecognition from './components/facerecognition/FaceRecognition';
+import Rank from './components/rank/Rank';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       <FaceRecognition />
     </div>
