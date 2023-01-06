c44c5a38c7640e0b02bd6d96e5698c0a0dba0917
Pavan Baloju <pavanbaloju9@gmail.com>
2023-01-06 19:07:50 +0530

    Update logo

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -4,10 +4,10 @@ import brain from './brain.png';
 
 const Logo = () => {
     return (
-        <div className="ma4 t0">
-            <Tilt className='Tilt br2 shadow-2' options={{max: 55}} style={{height: 150 , width: 150}}>
-                <div className='Tilt-inner'>
-                    <img alt='brain' src={brain} style={{height: 150 , width: 150}}/>
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3">
+                    <img style={{ paddingTop: '5px' }} alt='logo' src={brain} />
                 </div>
             </Tilt>
         </div>
