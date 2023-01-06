bf28cfd3b68bef861d55a8f0b09004525d6a6cb7
Pavan Baloju <pavanbaloju9@gmail.com>
2023-01-06 14:37:59 +0530

    Add image link form

--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -1,7 +1,19 @@
+import './ImageLinkForm.css'
+
 const ImageLinkForm = () => {
     return (
         <div>
-
+            <p className="f3">
+                {'This magic brain will detect faces in your pictures. Give it a try!'}
+            </p>
+            <div className="center">
+                <div className="pa4 br4 shadow-5 form center">
+                    <input className="f4 pa2 w-70 center" type='tex'/>
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+                        Detect face!
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
