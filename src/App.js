54269cfd07fb3b1758c5e8c1dcb82f9a2097a3ed
Pavan Baloju <pavanbaloju9@gmail.com>
2023-01-06 15:05:07 +0530

    Add particles-bg

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
 import FaceRecognition from './components/facerecognition/FaceRecognition';
 import Rank from './components/rank/Rank';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="circle" bg={true} num="30"/>
       <Navigation />
       <Logo />
       <Rank />
